5bca1f7a8a3277e40406489f0264640e0741897a
Ines Ivanova <ines.iv.ivanova@gmail.com>
2021-05-20 18:26:42 +0300

    Initial commit

---
+++ 1_stacks_and_queues/stack_demo.py
@@ -0,0 +1,26 @@
+def eat():
+    prepare_meal()
+    place_on_table()
+    grab_forks()
+    chew()
+
+
+def prepare_meal():
+    print("Cooking...")
+
+
+def place_on_table():
+    print("Remove from the oven and place it on the table")
+
+
+def grab_forks():
+    print("Looking for forks. Place them on the table")
+
+
+def chew():
+    print("Nom nom...")
+
+
+eat()
+
+
